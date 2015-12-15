c6eaee88d76464f0cbe24e5eca2bd22ef484c587
Melvin Carvalho <melvincarvalho@gmail.com>
2015-12-15 14:51:40 +0100

    Mysql schema

---
+++ schema/mysql/create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE CREDIT (
+  `@id` TEXT,
+  `source` TEXT,
+  `amount` REAL,
+  `currency` VARCHAR(255) DEFAULT 'https://w3id.org/cc#bit',
+  `destination` TEXT,
+  `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `context` TEXT,
+  `description` TEXT,
+  `wallet` TEXT
+);
+
+CREATE TABLE LEDGER (
+  `source` TEXT,
+  `amount` REAL,
+  `wallet` TEXT
+);
