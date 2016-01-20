d61ee8a64bb47bd9b67bdac0f430d083feff8615
Melvin Carvalho <melvincarvalho@gmail.com>
2016-01-20 19:01:37 +0100

    reputation

--- bin/reputation.js
+++ bin/reputation.js
@@ -1,93 +1,15 @@
 #!/usr/bin/env node
 
 // requires
-var Sequelize = require('sequelize');
-
-/**
- * setup database
- * @param  {string} dialect type of db mysql|sqlite
- * @param  {string} storage file used for sqlite, default ./credit.db
- * @return {Object} sequelize db object
- */
-function setupDB(config) {
-  var sequelize;
-  var defaultStorage = 'credit.db';
-
-  if (config.dialect === 'sqlite') {
-    if (!config.storage) {
-      config.storage = defaultStorage;
-    }
-
-    sequelize = new Sequelize(config.database, config.username, config.password, {
-      host: config.host,
-      dialect: config.dialect,
-      storage: config.storage,
-      logging: false,
-
-      pool: false
-
-    });
-  } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, {
-      host: config.host,
-      dialect: config.dialect,
-      logging: false,
-
-      pool: false
-
-    });
-  }
-  return sequelize;
-}
+var webcredits = require('../lib/webcredits.js');
 
 
 /**
- * get reputation
- * @param  {Object} sequelize db object
- */
-function getReputation(source, sequelize, config) {
-  var coinbase = 'https://w3id.org/cc#coinbase';
-  var currency = 'https://w3id.org/cc#bit';
-  var initial  = 1000000;
-
-  if (!config.wallet) {
-    config.wallet = null;
-  }
-
-  var coinbaseSql = 'Select sum(amount) amount from Credit where destination = :source and wallet = :wallet ;';
-
-  sequelize.query(coinbaseSql,  { replacements: { wallet: config.wallet, source: source } }).then(function(res) {
-    return res;
-  }).catch(function(err){
-    console.log('Reputation Failed.', err);
-  }).then(function(res) {
-    if (res[0][0]) {
-      console.log(res[0][0].amount);
-      sequelize.close();
-    }
-  });
-}
-
-/**
- * reputation function
- * @param  {Object} config [description]
- */
-function reputation(source, config) {
-  // vars
-  var sequelize;
-
-  // run main
-  sequelize = setupDB(config);
-  var res = getReputation(source, sequelize, config);
-}
-
-
-/**
- * version as a command
- */
+* version as a command
+*/
 function bin(argv) {
   // setup config
-  var config = require('./dbconfig.js');
+  var config = require('../config/dbconfig.js');
 
   var source = process.argv[2];
 
@@ -96,12 +18,11 @@ function bin(argv) {
     process.exit(-1);
   }
 
-  reputation(source, config);
+  webcredits.reputation(source, config);
+
 }
 
 // If one import this file, this is a module, otherwise a library
 if (require.main === module) {
   bin(process.argv);
 }
-
-module.exports = reputation;
